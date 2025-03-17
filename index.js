61b64aa0a08d9563b3e796bdaaaa8d6251710f06
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-17 20:01:01 +0600

    user api added

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true,
+  optionalSuccessStatus: 200
+}
+
+
+// middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cy3cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // collections
+    const dbCollection = client.db('scholarshipDB')
+    const userCollection = dbCollection.collection('users');
+
+    // generate jwt
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      // create token
+      const token = jwt.sign(email, process.env.SECRET_KEY, {
+        expiresIn: '3h',
+      })
+      // console.log(token);
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    // save or update user in db
+    app.post('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email }
+      const user = req.body;
+      // check if user exists in db
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist)
+      }
+      const result = await userCollection.insertOne({
+        ...user,
+        role: 'User',
+        timestamp: Date.now(),
+      })
+      res.send(result)
+    })
+
+    // logOut || clear cookie from browser
+    app.get('/logOut', async (req, res) => {
+      res.
+        clearCookie('token', {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send("Scholarship Management Server is running!")
+})
+
+app.listen(port, () => {
+  console.log(`Scholarship Management server is running on port ${port}`);
+})
