1ce40fca8a88798d32bce5d23d84d84d43ec509f
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-18 19:47:20 +0600

    scholarship details api added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cy3cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -106,6 +106,14 @@ async function run() {
     app.get('/scholarship', async(req, res)=> {
       const cursor = scholarshipCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // scholarship details api
+    app.get('/scholarship/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await scholarshipCollection.findOne(query);
       res.send(result);
     })
 
